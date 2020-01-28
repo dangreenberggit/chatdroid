b4185ba70c93b4f18fc854dc7e2b2a74590bbdf3
dangreenberggit <dgreenberg1987@gmail.com>
2020-01-28 20:00:09 +0200

    commit

--- app.py
+++ app.py
@@ -1,23 +1,15 @@
 from flask import Flask, render_template, request
-from chatterbot import ChatBot
-from chatterbot.trainers import ChatterBotCorpusTrainer, ListTrainer
-from memes import prequel_quotes as memes
+import random
+from crap_memes import memes
 
 app = Flask(__name__)
 
-chatbot = ChatBot("R2bot", storage_adapter="chatterbot.storage.SQLStorageAdapter")
-trainer = ListTrainer(chatbot)
-trainer.train(memes)
-
-@app.route("/bot")
-def home():
-    return render_template("home.html")
 
 @route('/', method='GET')
 def new_chat()
     user_message = requests.args.get('message')
-    return str(chatbot.get_response(user_message))
-
+    bot_message = random.choice(memes)
+    return {"message": bot_message}
 
 if __name__ == "__main__":
     app.run()
---
+++ crap_memes.py
@@ -0,0 +1,12 @@
+memes = [
+    "Hello there!",
+    "So Uncivilized.",
+    "Oh I don't think so.",
+    "Sith Lords are our specialty!",
+    "It's over, I have the high ground.",
+    "You want to go home and rethink your life.",
+    "Your move!",
+    "the negotiations were short",
+    "In my experience there's no such thing as luck.",
+    "Wait a minute how did this happen? we're smarter than this.",
+]
